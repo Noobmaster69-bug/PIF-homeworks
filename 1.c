bd5494ac448a29032644829040903dfc34258b51
Noobmaster69-bug <78723263+Noobmaster69-bug@users.noreply.github.com>
2021-02-26 20:36:23 +0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
     int a;
-	printf("nhap chieu cao: ");
-	scanf("%d",&a);
-	for(int i=0; i<a; i++){
-		printf("\n       ");
-	    for(int j=0; j<a-i;j++){
-		    printf(" ");
-	    }
-	    for(int j=0; j<2*i+1;j++){
-		    printf("#");
-	    }
-	}
+    printf("nhap chieu cao: ");
+    scanf("%d",&a);
+    for(int i=0; i<a; i++){
+    	printf("\n       ");
+    	for(int j=0; j<a-i;j++){
+             printf(" ");
+    	}
+    for(int j=0; j<2*i+1;j++){
+             printf("#");
+    	}
+    }
 }
